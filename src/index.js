bc8f8e4940b00ed9a8898aef0fec7f9a548b37cf
Aeri <rjungdesign@gmail.com>
2020-10-06 20:58:34 -0400

    remove virtualized

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "./styles/index.css";
 import * as serviceWorker from "./serviceWorker";
-import "react-virtualized/styles.css";
-import "react-virtualized-tree/lib/main.css";
 
 ReactDOM.render(
   <React.StrictMode>
